8213abd6e81a1d95632a572026387b00cb89ece7
matheusdiass1 <matheus_oliveiradias@hotmail.com>
2025-01-29 11:09:16 -0300

    Primeiro Commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function generateNumber() {
+    const min = Math.ceil(document.querySelector(".input-entre").value)
+
+    const max = Math.floor(document.querySelector(".input-e").value)
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    alert(result)
+}
