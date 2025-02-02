aaaae53929ad0dc43d400c157212f4912c85b3f0
matheusdiass1 <matheus_oliveiradias@hotmail.com>
2025-02-02 10:49:43 -0300

    If valor minimo

--- scripts.js
+++ scripts.js
@@ -3,7 +3,11 @@ function generateNumber() {
 
     const max = Math.floor(document.querySelector(".input-e").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert('O Valor minimo tem que ser MENOR que o valor máximo')
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
+        alert(result)
+    }
 }
